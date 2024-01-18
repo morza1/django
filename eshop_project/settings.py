1887336ebe4dade635215d17f2121473d3eda93b
Ali <33001923+morza1@users.noreply.github.com>
2024-01-18 23:34:11 +0330

    Update settings.py

--- eshop_project/settings.py
+++ eshop_project/settings.py
@@ -88,12 +88,7 @@ WSGI_APPLICATION = 'eshop_project.wsgi.application'
 AUTH_USER_MODEL = 'account_module.User'
 LOGIN_URL = '/login'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
